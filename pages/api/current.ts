ffff02f2164dcf8b8da3b36e283049a2f10293c8
samiksha <samikshawankhade11@gmail.com>
2023-07-29 20:15:13 +0530

    initial commit

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -14,6 +14,6 @@ export default async function handler(
     return res.status(200).json(currentUser);
   } catch (error) {
     console.log(error);
-    return res.status(500).end();
+    return res.status(400).end();
   }
 }
